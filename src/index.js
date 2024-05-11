d85846e736901e7c1353b3aff46edcfe190b34b0
Harshitk816 <harshitkaushal816@gmail.com>
2024-05-11 08:55:22 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import connectDB from './db/index.js'
 import dotenv from 'dotenv'
+import { app } from './app.js'
+
+
+
 
 dotenv.config(
     {
@@ -8,7 +12,18 @@ dotenv.config(
 )
 
 
-connectDB();
+connectDB()
+.then(
+    app.on("error",(error)=>{
+        console.log("Error in Database Connection : " + error);
+    })
+)
+.then(
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running on port ${process.env.PORT||8000}`)
+    })
+)
+.catch(error=>console.log(`Error in DB Connection`, error)) 
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler=(reqHandler)=>{
+    (req, res, next)=>{
+        Promise.resolve(reqHandler(req, res, next))
+        .catch((err)=>next(err))
+    }
+
+}
+
+export {asyncHandler}
+
+// const asyncHandler=()=>{}
+// const asyncHandler=(func)=>{async()=>{}}
+
+
+// const asyncHandler=(fun)=>async(req, res, next)=>{
+//     try{
+//         await fun(req, res, next)
+//     }catch(error){
+//         res.status(err.code).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// }
